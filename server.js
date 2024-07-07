4308954a0caa9b2445d2ff11f9f3c5bd35ae29aa
Daniel <danialkarimi109@gmail.com>
2024-07-07 02:43:20 +0100

    fix the route problem in server

--- server.js
+++ server.js
@@ -19,14 +19,15 @@ const allowedOrigins = ['https://zingy-twilight-e56255.netlify.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Additional headers middleware
