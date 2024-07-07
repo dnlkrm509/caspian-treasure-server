c6add703d1f558cddd7373ee5c248b31197909c1
Daniel <danialkarimi109@gmail.com>
2024-07-07 17:19:30 +0100

    update cors settings

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(cors({
 
 // Additional headers middleware
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://zingy-twilight-e56255.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
