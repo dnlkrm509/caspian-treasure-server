7a5d4851c17ea152ffa0d139319142108f30e5c9
Danial <danial@email.com>
2024-07-16 18:48:21 +0100

    modify mysql tables

--- server.js
+++ server.js
@@ -64,37 +64,53 @@ async function initializeDatabase() {
   )`;
 
   const q1 = `
-  CREATE TABLE IF NOT EXISTS carts (
+  CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT NOT NULL,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(255) NOT NULL,
-    amount INT NOT NULL,
-    price DECIMAL(6, 2) NOT NULL,
-    UID VARCHAR(36) NOT NULL,
-    totalAmount DECIMAL(8, 2) DEFAULT 0 NOT NULL
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL DEFAULT 'example@example.com' NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    zip VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL
   )`;
 
   const q2 = `
-  CREATE TABLE IF NOT EXISTS orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+  CREATE TABLE IF NOT EXISTS carts (
     product_id INT NOT NULL,
-    product_name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
     amount INT NOT NULL,
-    price DECIMAL(6, 2) NOT NULL,
-    customer_name VARCHAR(255) NOT NULL,
-    customer_email VARCHAR(255) NOT NULL DEFAULT 'example@example.com',
-    customer_address VARCHAR(255) NOT NULL,
-    customer_city VARCHAR(255) NOT NULL,
-    customer_state VARCHAR(255) NOT NULL,
-    customer_zip VARCHAR(255) NOT NULL,
-    customer_country VARCHAR(255) NOT NULL,
-    order_id VARCHAR(36) NOT NULL DEFAULT '123',
-    totalAmount DECIMAL(8, 2) NOT NULL DEFAULT 0
+    totalAmount Decimal(7, 2) DEFAULT 0,
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    PRIMARY KEY(product_id, user_id)
   )`;
 
   const q3 = `
+  CREATE TABLE IF NOT EXISTS customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+  )`;
+
+  const q4 = `
+  CREATE TABLE IF NOT EXISTS orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    confirmation VARCHAR(36) NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES customers(id)
+  )`;
+
+  const q5 = `
+  CREATE TABLE IF NOT EXISTS order_detail (
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY(order_id) REFERENCES orders(id),
+    FOREIGN KEY(product_id) REFERENCES products(id)
+  )`;
+
+  const q6 = `
   CREATE TABLE IF NOT EXISTS message_from (
     id INT AUTO_INCREMENT PRIMARY KEY,
     subject VARCHAR(255) NOT NULL,
@@ -103,23 +119,14 @@ async function initializeDatabase() {
     message VARCHAR(255) NOT NULL
   )`;
 
-  const q4 = `
+  const q7 = `
   CREATE TABLE IF NOT EXISTS message_to (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     subject VARCHAR(255) NOT NULL,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL DEFAULT 'example@example.com',
-    order_id VARCHAR(36) NOT NULL DEFAULT '123',
-    address VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    zip VARCHAR(255) NOT NULL,
-    country VARCHAR(255) NOT NULL,
+    customer_id INT NOT NULL,
     product_id INT NOT NULL,
-    product_name VARCHAR(255) NOT NULL,
-    amount INT NOT NULL,
-    price DECIMAL(6, 2) NOT NULL,
-    totalAmount DECIMAL(8, 2) NOT NULL DEFAULT 0
+    FOREIGN KEY(product_id) REFERENCES products(id),
+    FOREIGN KEY(customer_id) REFERENCES customers(id),
+    PRIMARY KEY(product_id, customer_id)
   )`;
   
   let connection;
@@ -130,6 +137,9 @@ async function initializeDatabase() {
     await connection.execute(q2);
     await connection.execute(q3);
     await connection.execute(q4);
+    await connection.execute(q5);
+    await connection.execute(q6);
+    await connection.execute(q7);
   } catch (err) {
     console.error('Error initializing database:', err.stack);
   } finally {
@@ -162,7 +172,16 @@ app.get('/api/products', async (req, res) => {
 });
 
 app.get('/api/cart-products', async (req, res) => {
-  const q = 'SELECT * FROM carts';
+  const q = `
+  SELECT users.id as userID,
+    products.id, products.name, products.description, products.price,
+    carts.amount, carts.totalAmount
+    FROM carts
+    INNER JOIN users ON
+    carts.user_id = users.id
+    INNER JOIN products ON
+    carts.product_id = products.id
+  `;
 
   let connection;
   
@@ -181,39 +200,78 @@ app.get('/api/cart-products', async (req, res) => {
 
 });
 
-app.post('/api/cart-products', async (req, res) => {
-  const { newProduct, cart, totalAmount } = req.body;
+app.post('/api/add-user', async (req, res) => {
+  const { name, password, email, address, city, state, zip, country } = req.body;
 
   let connection;
 
   const query = `
-  INSERT IGNORE INTO carts (
-    product_id,
+  INSERT INTO users (
     name,
-    description,
+    password,
+    email,
+    address,
+    city,
+    state,
+    zip,
+    country,
+  )
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const userValues = [
+    name,
+    password,
+    email,
+    address,
+    city,
+    state,
+    zip,
+    country
+  ];
+  
+
+  try {
+    connection = await getPool().getConnection();
+    
+    const [result] = await connection.execute(query, userValues);
+    console.log('Data inserted:', result);
+    
+    res.status(200).json({ message: 'user added!' });
+  } catch (err) {
+    console.error('Error inserting new user:', err.stack);
+    res.status(500).json({ message: 'Failed to add new user!' });
+  } finally {
+    if (connection) {
+      connection.release();
+    }
+  }
+  
+});
+
+app.post('/api/cart-products', async (req, res) => {
+  const { newProduct, userId, totalAmount } = req.body;
+
+  let connection;
+  
+  const query = `
+  INSERT INTO carts (
+    user_id,
+    product_id,
     amount,
-    price,
-    UID,
     totalAmount
   )
-  VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  VALUES (?, ?, ?, ?)`;
   const values = [
+    userId,
     newProduct.product_id,
-    newProduct.name,
-    newProduct.description,
     newProduct.amount,
-    newProduct.price
+    totalAmount
   ];
 
   try {
     connection = await getPool().getConnection();
-    if (cart) {
-      const [result] = await connection.execute(query, [...values, cart.UID, totalAmount]);
-      console.log('Data inserted:', result);
-    } else {
-      const [result] = await connection.execute(query, [...values, uuidv4(), totalAmount]);
-      console.log('Data inserted:', result);
-    }
+    
+    const [result] = await connection.execute(query, values);
+    console.log('Data inserted:', result);
     
     res.status(200).json({ message: 'Cart product/(s) added!' });
   } catch (err) {
@@ -229,31 +287,31 @@ app.post('/api/cart-products', async (req, res) => {
 
 app.put('/api/cart-products/:id', async (req, res) => {
   const productId = req.params.id;
-  const { newProduct, totalAmount } = req.body;
+  const { newProduct, userId, totalAmount } = req.body;
 
   let connection;
 
-  const query = 'UPDATE carts SET amount = ?, totalAmount = ? WHERE product_id = ?';
-  const query1 = 'UPDATE carts SET totalAmount = ? WHERE product_id = ?';
-  const q = 'SELECT * FROM carts';
+  const query = 'UPDATE carts SET amount = ?, totalAmount = ? WHERE product_id = ? AND user_id = ?';
+  const query1 = 'UPDATE carts SET totalAmount = ? WHERE product_id = ? AND user_id = ?';
+  const q = 'SELECT * FROM carts WHERE user_id = ?';
 
   try {
     connection = await getPool().getConnection();
     if (newProduct) {
-      const [result] = await connection.execute(query, [ newProduct.amount, totalAmount, productId ]);
+      const [result] = await connection.execute(query, [ newProduct.amount, totalAmount, productId, userId ]);
       if (result.affectedRows === 0) {
         return res.status(404).json({ message: 'Product not found' });
       }
       console.log('Product updated successfully: ', result);
     } else {
-      const [result] = await connection.execute(query1, [ totalAmount, productId ]);
+      const [result] = await connection.execute(query1, [ totalAmount, productId, userId ]);
       if (result.affectedRows === 0) {
         return res.status(404).json({ message: 'Product not found' });
       }
       console.log('Product updated successfully: ', result);
     }
     
-    const [rows, fields] = await connection.execute(q);
+    const [rows, fields] = await connection.execute(q, [ userId ]);
     res.status(200).json({rows});
   } catch (err) {
     console.error('Error updating data:', err.stack);
@@ -300,7 +358,14 @@ app.post('/api/message-from', async (req, res) => {
   let connection;
 
   
-  const q = 'INSERT INTO message_from (subject, from_name, from_email, message) VALUES (?, ?, ?, ?)';
+  const q = `
+  INSERT INTO message_from (
+    subject,
+    from_name,
+    from_email,
+    message
+  )
+  VALUES (?, ?, ?, ?)`;
   const values = [ data.subject, data.from_name, data.from_email, data.message ];
 
   try {
