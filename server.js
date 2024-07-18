f14102bf27a4c54ff42b7802203d09b860b9d39c
Danial <danial@email.com>
2024-07-18 13:54:57 +0100

    correct requests to server

--- server.js
+++ server.js
@@ -278,13 +278,13 @@ app.post('/api/users', async (req, res) => {
 
 app.post('/api/cart-products', async (req, res) => {
   const { newProduct, userId, user, totalAmount } = req.body;
+
+  console.log(newProduct, newProduct[0].product_id, userId, user, totalAmount)
   
   // Check if newProduct is an array or not properly structured
   if (!newProduct || !Array.isArray(newProduct) || newProduct.length === 0) {
     return res.status(400).json({ message: 'Invalid request. newProduct must be a non-empty array.' });
   }
-
-  console.log(newProduct, newProduct[0].product_id, userId, user, totalAmount)
 
   // Extract product_id and amount from the first item in newProduct array
   const productIdValue = newProduct[0].product_id;
